af5b6716a0c33d679c66e0dc14ccc038e1e2d3de
Shahar Kaminsky <sk@matterlabs.dev>
2023-09-28 09:56:47 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # get/update the source code and build bellman-cuda library
 if cd era-bellman-cuda; then git pull; else git clone https://github.com/matter-labs/era-bellman-cuda.git --branch main era-bellman-cuda; fi
-cmake -Sera-bellman-cuda/build -Sbellman-cuda/ -DCMAKE_BUILD_TYPE=Release
+cmake -Bera-bellman-cuda/build -Sera-bellman-cuda/ -DCMAKE_BUILD_TYPE=Release
 cmake --build era-bellman-cuda/build/
 
 # build testing library
