dbecd64d1ce0961247cb6a4699b26309dc04f304
Shahar Kaminsky <sk@matterlabs.dev>
2023-09-28 09:38:55 +0300

    Using public bellman-cuda.

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # get/update the source code and build bellman-cuda library
-if cd bellman-cuda; then git pull; else git clone https://github.com/matter-labs/bellman-cuda.git --branch dev bellman-cuda; fi
-cmake -Bbellman-cuda/build -Sbellman-cuda/ -DCMAKE_BUILD_TYPE=Release
-cmake --build bellman-cuda/build/
+if cd era-bellman-cuda; then git pull; else git clone https://github.com/matter-labs/era-bellman-cuda.git --branch main era-bellman-cuda; fi
+cmake -Bera-bellman-cuda/build -Sbellman-cuda/ -DCMAKE_BUILD_TYPE=Release
+cmake --build era-bellman-cuda/build/
 
 # build testing library
-export BELLMAN_CUDA_DIR=$PWD/bellman-cuda
+export BELLMAN_CUDA_DIR=$PWD/era-bellman-cuda
 cargo test --package api-testing --release --no-run
 # find target/release/deps -type f -executable -iname 'testing-*'
