1f91c320026b88b85c61de4450c0c90a3b3a3eb0
Igor Aleksanov <popzxc@yandex.ru>
2024-08-12 14:58:02 +0400

    crates.io 0.150.0 release (#16)

--- prover-service/src/simple/mod.rs
+++
@@ -1,9 +0,0 @@
-use super::*;
-
-pub mod simple_artifact_manager;
-// pub mod http_artifact_manager;
-pub mod simple_job_manager;
-
-pub use simple_artifact_manager::*;
-// pub use http_artifact_manager::*;
-pub use simple_job_manager::*;
